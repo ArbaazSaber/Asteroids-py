98ff1d2bf0919b80ee60246ef240f5e990c6055b
Arbaaz Saber <arbaazfb26@gmail.com>
2026-04-13 21:27:41 +0530

    Drew Triangle on Screen

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pygame
-from logger import log_state
 
+from logger import log_state
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
+
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -18,7 +23,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        game_tick = game_clock.tick(60)
+        dt = game_tick / 1000
 
 if __name__ =="__main__":
     main()
